4728deba1ea523c9909391382c957a69d156e812
sieteunoseis <jeremy.worden@gmail.com>
2026-03-27 09:19:31 -0700

    Changes

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: process.env.VITE_API_URL || "https://cucm-cdr.tuce.ohsu.edu",
+        target: process.env.VITE_API_URL || "http://localhost:8080",
         changeOrigin: true,
         secure: false,
       },
